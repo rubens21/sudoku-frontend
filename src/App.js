aa77af80b399b106b7bdf1c0cd02b868760bbd19
Rubens Silva <rubens21@gmail.com>
2018-10-20 21:01:42 -0400

    updating ignore and displaying the grid.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+const BACKEND = `http://localhost/sudoku/board`;
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      numbers: []
+    };
   }
+
+  loadNewBoard() {
+    fetch(BACKEND)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          this.setState({
+            isLoaded: true,
+            numbers: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  componentDidMount() {
+    this.loadNewBoard()
+  }
+
+  render() {
+    const {error, isLoaded, numbers} = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return RenderBoard(numbers);
+    }
+  }
+
+}
+
+function RenderBoard(numbers) {
+  let rows = [];
+  for (let i = 0; i < 9; i++){
+    let rowID = `row${i}`;
+    let cell = [];
+    const indexPot = 9 * i;
+    for (let idx = 0; idx < 9; idx++){
+      let cellID = `cell${i}-${idx}`;
+      cell.push(<td key={cellID} id={cellID}>{numbers[idx+indexPot]}</td>)
+    }
+    rows.push(<tr key={i} id={rowID}>{cell}</tr>)
+  }
+  return <table id="simple-board">
+    <tbody>
+    {rows}
+    </tbody>
+  </table>;
 }
 
 export default App;
